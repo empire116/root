431ed19e922e01e9ec6336862f8bf53c4ca54ab5
empire116 <49943366+empire116@users.noreply.github.com>
2021-07-07 15:18:50 +0700

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "# Script Auto Install SSH & OpenVPN             #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & 10 Buster 64 bit               #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Modified by FCK WORLD                         #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#
+echo -e "# Google                                        #"
+echo -e "# Embex.online                                  #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
